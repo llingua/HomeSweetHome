f5a121cdf91d473c6fe57f5714a08203dfbf51a2
Lorenzo Lingua <lorenzo.lingua@gmail.com>
2026-01-07 23:28:44 +0100

    Use relative asset paths in add-on

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isAddon = process.env.ADDON === 'true' || process.env.ADDON === '1';
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     useWasmBinary: true,
   },
+  assetPrefix: isAddon ? './' : undefined,
 };
 
 export default nextConfig;
